b4f518f5538fd38d3d6ced6e263a61f39af20425
Leon_ <65647418+LeonAndreasen@users.noreply.github.com>
2020-11-04 14:13:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-//const n = 2000
+const secret = randomInt()
 function randomInt(){
 
 return Math.floor(Math.random() * 100);
 }
-console.log(randomInt())
+
 
 function getUserGuess() {
 const stringValue = document.getElementById("user-input").value
@@ -13,12 +13,18 @@ document.addEventListener("keyup", function (event) {
     if (event.key === 'Enter') {
         const guess = getUserGuess()
         console.log(guess)
+        if (guess > secret) {
+setMessage("Too High!")}
+if (guess < secret){
+    setMessage("Too Low!")
     }
+    if (guess === secret){
+        setMessage("You are correct")
+    }
+}
 })
+
  function setMessage(msg){
-     if (guess  === randomInt) {
-     alert ("You won!")
-     } else {
-         
-     }
- }
+     document.getElementById("message").innerText = msg
+    }  
+ 
