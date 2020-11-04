369e658418e9327460e4295b4a2c6506c73b7a6c
Leon_ <65647418+LeonAndreasen@users.noreply.github.com>
2020-11-04 12:33:46 +0100

    test

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Read instructions. And remove this line.
+//const n = 2000
+function randomInt(){
+
+return Math.floor(Math.random() * 100);
+}
+console.log(randomInt())
+
+function getUserGuess() {
+const stringValue = document.getElementById("user-input").value
+return parseInt(stringValue, 10)
+}
+document.addEventListener("keyup", function (event) {
+    if (event.key === 'Enter') {
+        const guess = getUserGuess()
+        console.log(guess)
+    }
+})
+ function setMessage(msg){
+     if (guess  === randomInt) {
+     alert ("You won!")
+     } else {
+         
+     }
+ }
